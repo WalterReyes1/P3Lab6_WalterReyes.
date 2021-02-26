d0e7788d4b3b4658e7b74a1991ce585ff270cdc9
walter reyes <walter.reyes@cambridgehn.com>
2021-02-26 14:45:30 -0600

    Clases terminadas

---
+++ album.cpp
@@ -0,0 +1,40 @@
+#include "canciones.hpp"
+#include "album.hpp"
+#include <string>
+#include <vector>
+using namespace std;
+album::album() {
+
+}
+
+album::album(string _nombre,string _artistas, vector<canciones> _Canciones) {
+	this->nombre = _nombre;
+	this->artistas=_artistas;
+	this->Canciones = _Canciones;
+
+}
+
+string album::getNombre() {
+	return this->nombre;
+}
+
+void album::setNombre(string _nombre) {
+	this->nombre = _nombre;
+}
+string album::getArtistas() {
+	return this->artistas;
+}
+
+void album::setArtistas(string _artistas) {
+	this->artistas = _artistas;
+}
+
+vector<canciones>album::getCanciones() {
+	return this->Canciones;
+}
+
+void album::setCanciones(vector<canciones>_Canciones) {
+	this->Canciones = _Canciones;
+}
+
+
--- playlist.cpp
+++ playlist.cpp
@@ -3,34 +3,30 @@
 #include <string>
 #include <vector>
 using namespace std;
-playlist::playlist(){
-  // cout<<"llamaron al vacio" << endl;
+playlist::playlist() {
+
 }
 
-playlist::playlist(string _nombre, vector<canciones> _Canciones){
-  this->nombre = _nombre;
-  this->Canciones = _Canciones;
-  
+playlist::playlist(string _nombre, vector<canciones> _Canciones) {
+	this->nombre = _nombre;
+	this->Canciones = _Canciones;
+
 }
 
-
- 
-
-
-string playlist::getNombre(){
-  return this->nombre;
+string playlist::getNombre() {
+	return this->nombre;
 }
 
-void playlist::setNombre(string _nombre){
-  this->nombre = _nombre;
+void playlist::setNombre(string _nombre) {
+	this->nombre = _nombre;
 }
 
-vector<canciones>playlist::getPlaylist(){
-  return this->Canciones;
+vector<canciones>playlist::getPlaylist() {
+	return this->Canciones;
 }
 
-void playlist::setPlaylist(vector<canciones>_Canciones){
-  this->Canciones = _Canciones;
+void playlist::setPlaylist(vector<canciones>_Canciones) {
+	this->Canciones = _Canciones;
 }
 
 
