6c8b331d14ba90a77e7eabab117d1830e9f55ab9
walter reyes <walter.reyes@cambridgehn.com>
2021-02-26 13:25:39 -0600

    Inicio

---
+++ genero.hpp
@@ -0,0 +1,14 @@
+#ifndef ASIENTO_HPP
+#define ASIENTO_HPP
+#include<iostream>
+#include<string>
+using namespace std;
+class genero {
+	private: 
+	string nombre;
+  public:
+  genero();
+  genero(string);
+  ~genero();
+};
+#endif
